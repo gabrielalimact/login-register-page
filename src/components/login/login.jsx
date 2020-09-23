325679f609453fa37cc5bbdf55227b9c41003177
Gabriela <54540040+gabrielalimact@users.noreply.github.com>
2020-09-23 09:43:43 -0300

    Add files via upload

---
+++ src/components/login/login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="base-container" ref={this.props.containerRef}>
+            <div className="header">LOGIN</div>
+            <div className="content">
+                <div className="image">
+                    <img src={loginImg} alt="login"/>
+                </div>
+                <div className="form">
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password"/>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="footer">
+                <button type="button" className="btn">
+                    Login
+                </button>
+            </div>
+            </div>
+            
+        );
+    }
+}
